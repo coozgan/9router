reddb-io/red-router

feat(combo): add Smart routing strategy via Jev complexity classifier
Adds a fourth combo strategy ("smart") alongside fallback / round-robin /
fusion. Before model ordering it classifies the current coding task into a
complexity tier with TypeSafe's Jev "System One" decision model, then
reorders the combo so the tier-appropriate model leads. The existing
availability/quota fallback ladder and capability auto-switch are untouched
- this only changes which model is tried first.

9router routes on availability (quota, rate-limit, health) and capability
(vision/pdf) but has no notion of task complexity. Jev fills that axis:
typed, calibrated probabilities at ~300ms and ~$0.0000185 per decision,
cheap enough to run inline on every request for every client.

Jev is NOT a provider or combo member - it does not generate text, so it
cannot be a handleSingleModel target. It is a routing-policy input, the
same role detectRequiredCapabilities plays.

Fail-open by contract (same rule as rtk/): classifyTier returns null on
no key, empty state, timeout (trips a 30s circuit breaker), non-200,
malformed body, unknown tier, or confidence below smartMinConfidence. On
null the combo keeps its existing order and routes normally, so the
classifier can never block a request.

Capability auto-switch runs after tier reordering, so a hard capability
(vision/pdf) still outranks a tier preference - picking a non-vision model
would drop image data, so that precedence is deliberate.

- open-sse/config/jev.js: all Jev constants (endpoint, tiers, criteria,
  state budget, timeout, breaker cooldown, min confidence, spend pricing).
- open-sse/services/jevClassifier.js: classifyTier + buildJevState + the
  process-local circuit breaker. fetchImpl/now are injectable for tests.
  Builds a bounded state from the current user turn only, reusing
  trailingUserItems, so tool_result blobs never reach Jev.
- open-sse/services/combo.js: reorderModelsForTier (pure, network-free);
  trailingUserItems is now ex

src/sse/handlers/chat.js
@@ -17,7 +17,8 @@ import { DEFAULT_HEADROOM_URL } from "@/lib/headroom/detect";
 import { getTransform as getPxpipeTransform } from "@/lib/pxpipe/loader.js";
 import { appendPxpipeEvent } from "@/lib/pxpipe/events.js";
 import { createErrorContext, errorResponse, responseFromRoutingCandidate, withRequestId } from "open-sse/utils/error.js";
-import { handleComboChat, handleFusionChat, detectRequiredCapabilities } from "open-sse/services/combo.js";
+import { handleComboChat, handleFusionChat, detectRequiredCapabilities, reorderModelsForTier } from "open-sse/services/combo.js";
+import { classifyTier } from "open-sse/services/jevClassifier.js";
 import { augmentModelsWithCapacityAdapter, withCapacityAdapterStripping, getActiveAdapterStrategy } from "open-sse/services/capacityAdapter.js";
 import { handleBypassRequest } from "open-sse/utils/bypassHandler.js";
 import { HTTP_STATUS } from "open-sse/config/runtimeConfig.js";
@@ -139,7 +140,37 @@ export async function handleChat(request, clientRawRequest = null, options = {})
     }
 
     const comboStickyLimit = settings.comboStickyRoundRobinLimit;
-    log.info("CHAT", `Combo "${cleanComboName}" with ${augmentedModels.length} models (strategy: ${comboStrategy}, sticky: ${comboStickyLimit})`);
+    // Smart routing: classify task complexity with Jev (TypeSafe) and reorder the
+    // combo so the tier-appropriate model leads. Fail-open — on any classifier
+    // miss we keep `augmentedModels` as-is and fall through to normal fallback.
+    // The availability/quota ladder + capability auto-switch below are untouched.
+    let effectiveStrategy = comboStrategy;
+    if (comboStrategy === "smart") {
+      const smartCfg = comboStrategies[cleanComboName] || {};
+      const tierMap = smartCfg.smartTiers;
+      const classified = await classifyTier({
+        body,
+        log,
+        criteria: smartCfg.smartCriteria,
+        instructions: smartCfg.smartInstructions,
+        minConfidence: smartCfg.smartMinConfidence,
+        timeoutMs: smartCfg.smartTimeoutMs,
+      });
+      if (classified && tierMap) {
+        const reordered = reorderModelsForTier(augmentedModels, classified.tier, tierMap);
+        if (reordered[0] !== augmentedModels[0]) {
+          log.info("CHAT", `Combo "${cleanComboName}" smart-routing tier=${classified.tier} → ${reordered[0]}`);
+        }
+        augmentedModels.length = 0;
+        augmentedModels.push(...reordered);
+      } else {
+        log.info("CHAT", `Combo "${cleanComboName}" smart-routing: no confident tier — using default order`);
+      }
+      // Run the reordered chain through the normal fallback ladder.
+      effectiveStrategy = "fallback";
+    }
+
+    log.info("CHAT", `Combo "${cleanComboName}" with ${augmentedModels.length} models (strategy: ${comboStrategy}${effectiveStrategy !== comboStrategy ? `→${effectiveStrategy}` : ""}, sticky: ${comboStickyLimit})`);
     return handleComboChat({
       body,
       models: augmentedModels,
@@ -149,7 +180,7 @@ export async function handleChat(request, clientRawRequest = null, options = {})
       ),
       log,
       comboName: cleanComboName,
-      comboStrategy,
+      comboStrategy: effectiveStrategy,
       comboStickyLimit,
       errorContext,
     });

coozgan/9router

src/sse/handlers/chat.js
@@ -15,7 +15,8 @@ import { DEFAULT_HEADROOM_URL } from "@/lib/headroom/detect";
 import { getTransform as getPxpipeTransform } from "@/lib/pxpipe/loader.js";
 import { appendPxpipeEvent } from "@/lib/pxpipe/events.js";
 import { errorResponse, unavailableResponse } from "open-sse/utils/error.js";
-import { handleComboChat, handleFusionChat, detectRequiredCapabilities } from "open-sse/services/combo.js";
+import { handleComboChat, handleFusionChat, detectRequiredCapabilities, reorderModelsForTier } from "open-sse/services/combo.js";
+import { classifyTier } from "open-sse/services/jevClassifier.js";
 import { augmentModelsWithCapacityAdapter, withCapacityAdapterStripping, getActiveAdapterStrategy } from "open-sse/services/capacityAdapter.js";
 import { handleBypassRequest } from "open-sse/utils/bypassHandler.js";
 import { HTTP_STATUS } from "open-sse/config/runtimeConfig.js";
@@ -123,7 +124,38 @@ export async function handleChat(request, clientRawRequest = null) {
     }
 
     const comboStickyLimit = settings.comboStickyRoundRobinLimit;
-    log.info("CHAT", `Combo "${modelStr}" with ${augmentedModels.length} models (strategy: ${comboStrategy}, sticky: ${comboStickyLimit})`);
+
+    // Smart routing: classify task complexity with Jev (TypeSafe) and reorder the
+    // combo so the tier-appropriate model leads. Fail-open — on any classifier
+    // miss we keep `augmentedModels` as-is and fall through to normal fallback.
+    // The availability/quota ladder + capability auto-switch below are untouched.
+    let effectiveStrategy = comboStrategy;
+    if (comboStrategy === "smart") {
+      const smartCfg = comboStrategies[modelStr] || {};
+      const tierMap = smartCfg.smartTiers;
+      const classified = await classifyTier({
+        body,
+        log,
+        criteria: smartCfg.smartCriteria,
+        instructions: smartCfg.smartInstructions,
+        minConfidence: smartCfg.smartMinConfidence,
+        timeoutMs: smartCfg.smartTimeoutMs,
+      });
+      if (classified && tierMap) {
+        const reordered = reorderModelsForTier(augmentedModels, classified.tier, tierMap);
+        if (reordered[0] !== augmentedModels[0]) {
+          log.info("CHAT", `Combo "${modelStr}" smart-routing tier=${classified.tier} → ${reordered[0]}`);
+        }
+        augmentedModels.length = 0;
+        augmentedModels.push(...reordered);
+      } else {
+        log.info("CHAT", `Combo "${modelStr}" smart-routing: no confident tier — using default order`);
+      }
+      // Run the reordered chain through the normal fallback ladder.
+      effectiveStrategy = "fallback";
+    }
+
+    log.info("CHAT", `Combo "${modelStr}" with ${augmentedModels.length} models (strategy: ${comboStrategy}${effectiveStrategy !== comboStrategy ? `→${effectiveStrategy}` : ""}, sticky: ${comboStickyLimit})`);
     return handleComboChat({
       body,
       models: augmentedModels,
@@ -133,7 +165,7 @@ export async function handleChat(request, clientRawRequest = null) {
       ),
       log,
       comboName: modelStr,
-      comboStrategy,
+      comboStrategy: effectiveStrategy,
       comboStickyLimit
     });
   }